72c990b17786cbed578aafb64bc63c98bc0ba7ee
souf139 <soufianboushaba12@gmail.com>
2023-12-02 17:48:57 +0100

    this commit is for add the chart js to the dashboard

--- dashboard/header.php
+++ dashboard/header.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard</title>
-
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
